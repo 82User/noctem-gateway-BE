ddc04ad101ecc03554acf5a7ecf08684b19424e6
yjg1 <zxcvc9999@gmail.com>
2022-10-28 10:06:20 +0900

    first commit

---
+++ src/main/java/noctem/gatewayService/GatewayServiceApplication.java
@@ -0,0 +1,21 @@
+package noctem.gatewayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayServiceApplication {
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}
